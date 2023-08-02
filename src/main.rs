eb3e9c6620e7b2112d79a84542b8b0df59348a05
e.minetti <e.minetti@posteo.de>
2023-08-02 22:51:31 +0200

    First route for actix.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
