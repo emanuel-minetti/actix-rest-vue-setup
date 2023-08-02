8be24a82ac421b4bd896d5dcf21606ab45809842
e.minetti <e.minetti@posteo.de>
2023-08-03 00:50:36 +0200

    Started basic routing.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,42 @@ use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(hello))
+            .service(
+                web::scope("/login")
+                    .route("", web::post().to(hello_from_login))
+                    .route("", web::delete().to(hello_from_logout))
+                    .default_service(
+                        web::route()
+                            .to(HttpResponse::MethodNotAllowed),
+                    )
+            )
+            .service(
+                web::scope("/api")
+                    .route("/", web::get().to(hello_from_api))
+            )
+            .service(
+                web::scope("")
+                    .route("/{route}", web::get().to(hello_from_default))
+                    .route("/", web::get().to(hello_from_default))
+            )
     })
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn hello_from_api() -> impl Responder {
+    HttpResponse::Ok().body("Here is the API!")
+}
+
+async fn hello_from_login() -> impl Responder {
+    HttpResponse::Ok().body("Here is the Login API!")
+}
+
+async fn hello_from_logout() -> impl Responder {
+    HttpResponse::Ok().body("Here is the Logout API!")
+}
+
+async fn hello_from_default() -> impl Responder {
+    HttpResponse::Ok().body("Here is the Default API!")
 }
