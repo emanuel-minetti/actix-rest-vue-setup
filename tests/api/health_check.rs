3f16c8ec317651b3ec657cc7c6025d5b59c48aa9
Emanuel Minetti <e.minetti@posteo.de>
2023-08-09 00:25:59 +0200

    Server: Another test.

--- tests/api/health_check.rs
+++ tests/api/health_check.rs
@@ -1,4 +1,6 @@
 use crate::helpers::spawn_app;
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -14,5 +16,29 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-    // TODO assert static directory is accessible
+}
+
+#[tokio::test]
+async fn favicon_works() {
+    // Arrange
+    spawn_app().await;
+    let client = reqwest::Client::new();
+    let file = File::open("src/vue-client/public/favicon.ico")
+        .expect("Should be able to open 'favicon.ico'");
+    let mut file_reader = BufReader::new(file);
+    let mut file_buffer = Vec::new();
+    file_reader
+        .read_to_end(&mut file_buffer)
+        .expect("Should be able to read 'favicon.ico'");
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8080/favicon.ico")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    let response_bytes = response.bytes().await;
+    let response_buffer = response_bytes.expect("Should be able to open response answer");
+    assert_eq!(file_buffer, response_buffer);
 }
