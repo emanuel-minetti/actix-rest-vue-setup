481388ee85f6f3dab54f59800731b84d910eb4fc
Emanuel Minetti <e.minetti@posteo.de>
2023-08-13 23:31:37 +0200

    Client: Started using Bootstrap.

--- src/vue-client/src/App.vue
+++ src/vue-client/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header>
+  <button class="btn btn-danger">Test</button>
   <HelloWorld msg="You did it!" />
   <RouterView />
 </template>
