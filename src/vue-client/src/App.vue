3203b66aa856028826bebf1aa6b0e661a697468b
Emanuel Minetti <e.minetti@posteo.de>
2023-08-23 00:40:15 +0200

    Client: Continued on the footer.

--- src/vue-client/src/App.vue
+++ src/vue-client/src/App.vue
@@ -30,18 +30,21 @@
   </main>
   <footer class="outside-main mt-3">
     <div class="container-fluid">
-      <div class="footer-links">
-        <ul class="list-unstyled">
-          <li>
-            <RouterLink class="nav-link" to="/imprint">Imprint</RouterLink>
-          </li>
-          <li>
-            <RouterLink class="nav-link" to="/data-privacy">Data Privacy Declaration</RouterLink>
-          </li>
-          <li>
-            <a class="nav-link" href="https://example.com">Company home page</a>
-          </li>
-        </ul>
+      <div class="row align-items-center">
+        <div class="col">
+          <ul class="list-unstyled">
+            <li>
+              <RouterLink class="nav-link" to="/imprint">Imprint</RouterLink>
+            </li>
+            <li>
+              <RouterLink class="nav-link" to="/data-privacy">Data Privacy Declaration</RouterLink>
+            </li>
+            <li>
+              <a class="nav-link" href="https://example.com" target="_blank">Company home page</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-9 text-center">&copy; Example.com 2023</div>
       </div>
     </div>
   </footer>
@@ -65,5 +68,13 @@ import { RouterLink, RouterView } from 'vue-router'
 
 .router-link-active {
   @extend .fw-bold;
+}
+
+footer {
+  font-size: $font-size-sm;
+}
+
+footer .nav-link {
+  text-decoration: underline;
 }
 </style>
