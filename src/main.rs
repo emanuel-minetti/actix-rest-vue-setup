e0d5860281602c8892cce26f1c99ddd7b24aec13
Emanuel Minetti <e.minetti@posteo.de>
2023-10-03 01:54:11 +0200

    Continued using the pid file. Implemented `status`.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs::{File, remove_file};
+use std::path::Path;
 use std::process::exit;
 //use actix_rest_vue_setup::run;
 
+const PID_FILE_PATH: &str = "actix-rest-vue-setup.pid";
+
 #[tokio::main]
 async fn main() -> () {
+
 //async fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
@@ -13,25 +17,33 @@ async fn main() -> () {
     }
     match args[1].as_ref() {
         "start" => {start()},
-        "status" => {println!("Status")},
+        "status" => {status()},
         "stop" => {stop()},
         "restart" => {println!("Restarting")},
         _ => print_usage(&args[0])
     }
-    //run()?.await
 }
 
 fn start() -> () {
     println!("Starting");
-    let mut _pid_file = File::create("actix-rest-vue-setup.pid");
-    //let temp_file = temp_dir().push("actix-rest-vue-setup.pid");
+    let mut _pid_file = File::create(PID_FILE_PATH);
+    //run()?.await
 }
 
 fn stop() -> () {
     println!("Stopping");
-    remove_file("actix-rest-vue-setup.pid").expect("Couldn't remove pid file");
+    remove_file(PID_FILE_PATH).expect("Couldn't remove pid file");
+}
+
+fn status() -> () {
+    println!("Status: {}", if is_running() {"Running"} else {"Stopped"});
 }
 
 fn print_usage(called: &str) -> () {
     println!("Usage: {} (start | status | stop | restart)", called);
+}
+
+fn is_running() -> bool {
+    let pid_file_path = Path::new(PID_FILE_PATH);
+    pid_file_path.exists()
 }
