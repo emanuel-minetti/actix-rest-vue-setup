71242fc070d3fba10f4d1b773f5c1de046004b0b
Emanuel Minetti <e.minetti@posteo.de>
2023-10-06 00:44:14 +0200

    Linted and formatted.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
 use std::env;
-use std::fs::{File};
+use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
 use std::process::{Command, Stdio};
-use sysinfo::{System, SystemExt, Pid, ProcessExt, RefreshKind};
+use sysinfo::{Pid, ProcessExt, RefreshKind, System, SystemExt};
 
 const PID_FILE_PATH: &str = "actix-rest-vue-setup.pid";
 
-fn main() -> () {
+fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         print_usage(&args[0]);
         std::process::exit(0);
     }
     match args[1].as_ref() {
-        "start" => {start()},
-        "status" => {status()},
-        "stop" => {stop()},
-        "restart" => {restart()},
-        _ => print_usage(&args[0])
+        "start" => start(),
+        "status" => status(),
+        "stop" => stop(),
+        "restart" => restart(),
+        _ => print_usage(&args[0]),
     }
 }
 
-fn start() -> () {
+fn start() {
     println!("Starting");
     if is_running() {
         println!("Seems to be running. Consider stopping.");
         std::process::exit(0)
     }
-    let mut pid_file = File::create(PID_FILE_PATH)
-        .expect("Could not create pid file.");
+    let mut pid_file = File::create(PID_FILE_PATH).expect("Could not create pid file.");
     let child = Command::new("cargo")
         .arg("run")
         .arg("--bin")
@@ -39,12 +38,14 @@ fn start() -> () {
         .spawn()
         .expect("Failed to run the server.");
     let pid = child.id().to_string();
-    pid_file.write(pid.as_ref()).expect("Could not write to pid file.");
+    pid_file
+        .write_all(pid.as_ref())
+        .expect("Could not write to pid file.");
     println!("Started pid: {}", pid);
     std::process::exit(0)
 }
 
-fn stop() -> () {
+fn stop() {
     println!("Stopping");
     if !is_running() {
         println!("Seems to be not running. Try starting.");
@@ -52,12 +53,16 @@ fn stop() -> () {
     }
     let mut pid_file = File::open(PID_FILE_PATH).expect("Failed to open pid file.");
     let pid_file_content = &mut "".to_string();
-    pid_file.read_to_string(pid_file_content).expect("Failed to read pid file.");
-    let pid = pid_file_content.parse::<usize>()
+    pid_file
+        .read_to_string(pid_file_content)
+        .expect("Failed to read pid file.");
+    let pid = pid_file_content
+        .parse::<usize>()
         .expect("Pin file content should parse to a number");
-    let binding = System::new_with_specifics(
-        RefreshKind::with_processes(
-            RefreshKind::default(), Default::default()));
+    let binding = System::new_with_specifics(RefreshKind::with_processes(
+        RefreshKind::default(),
+        Default::default(),
+    ));
     let pid = Pid::from(pid);
     let process = binding.process(pid).expect("Pid process should exist.");
     process.kill();
@@ -65,11 +70,14 @@ fn stop() -> () {
     println!("Stopped pid: {}", pid);
 }
 
-fn status() -> () {
-    println!("Status: {}", if is_running() {"Running"} else {"Stopped"});
+fn status() {
+    println!(
+        "Status: {}",
+        if is_running() { "Running" } else { "Stopped" }
+    );
 }
 
-fn restart() -> () {
+fn restart() {
     println!("Restarting");
     if !is_running() {
         println!("Seems not to be running. Consider starting.");
@@ -79,7 +87,7 @@ fn restart() -> () {
     start();
 }
 
-fn print_usage(called: &str) -> () {
+fn print_usage(called: &str) {
     println!("Usage: {} (start | status | stop | restart)", called);
 }
 
--- src/vue-client/src/components/__tests__/Title.spec.ts
+++ src/vue-client/src/components/__tests__/Title.spec.ts
@@ -1,11 +1,11 @@
-import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
 
-import Title from "../Title.vue"
+import Title from '../Title.vue';
 
 describe('Title', () => {
-   it('renders properly', () => {
-     const wrapper = mount(Title, { props: { msg: 'Hello Vitest' } })
-     expect(wrapper.text()).toContain('Hello Vitest')
-   })
- })
+  it('renders properly', () => {
+    const wrapper = mount(Title, { props: { msg: 'Hello Vitest' } });
+    expect(wrapper.text()).toContain('Hello Vitest');
+  });
+});
