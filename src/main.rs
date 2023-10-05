4763125982471612a637d7e7599271e3271c1699
Emanuel Minetti <e.minetti@posteo.de>
2023-10-06 00:13:28 +0200

    Added checks to start and stop.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::{File};
 use std::io::{Read, Write};
 use std::path::Path;
-use std::process::{Command, exit, Stdio};
+use std::process::{Command, Stdio};
 use sysinfo::{System, SystemExt, Pid, ProcessExt, RefreshKind};
 
 const PID_FILE_PATH: &str = "actix-rest-vue-setup.pid";
@@ -11,7 +11,7 @@ fn main() -> () {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         print_usage(&args[0]);
-        exit(0);
+        std::process::exit(0);
     }
     match args[1].as_ref() {
         "start" => {start()},
@@ -24,6 +24,10 @@ fn main() -> () {
 
 fn start() -> () {
     println!("Starting");
+    if is_running() {
+        println!("Seems to be running. Consider stopping.");
+        std::process::exit(0)
+    }
     let mut pid_file = File::create(PID_FILE_PATH)
         .expect("Could not create pid file.");
     let child = Command::new("cargo")
@@ -37,11 +41,15 @@ fn start() -> () {
     let pid = child.id().to_string();
     pid_file.write(pid.as_ref()).expect("Could not write to pid file.");
     println!("Started pid: {}", pid);
-    exit(0)
+    std::process::exit(0)
 }
 
 fn stop() -> () {
     println!("Stopping");
+    if !is_running() {
+        println!("Seems to be not running. Try starting.");
+        std::process::exit(0)
+    }
     let mut pid_file = File::open(PID_FILE_PATH).expect("Failed to open pid file.");
     let pid_file_content = &mut "".to_string();
     pid_file.read_to_string(pid_file_content).expect("Failed to read pid file.");
