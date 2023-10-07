faeb4172fbc20261ad10aa3633ff5cb0e9990b97
Emanuel Minetti <e.minetti@posteo.de>
2023-10-07 22:30:05 +0200

    Running and stopping via cargo-make works.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::process::{Command, Stdio};
 use sysinfo::{Pid, ProcessExt, RefreshKind, System, SystemExt};
 
 const PID_FILE_PATH: &str = "actix-rest-vue-setup.pid";
+const PROCESS_NAME: &str = "actix-rest-vue-setup-run";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,13 +15,18 @@ fn main() {
         2 => match args[1].as_ref() {
             "start" => start(false),
             "status" => status(),
-            "stop" => stop(),
+            "stop" => stop(false),
             "restart" => restart(),
             _ => print_usage(&args[0]),
         },
         3 => {
-            if &args[1] == "start" && &args[2] == "--force" {
-                start(true);
+                if &args[2] == "--force" {
+                    match args[1].as_ref() {
+                        "start" => start(true),
+                        "stop" => stop(true),
+                        _ => print_usage(&args[0]),
+                    }
+
             } else {
                 print_usage(&args[0])
             }
@@ -32,7 +38,7 @@ fn main() {
 fn start(forced: bool) {
     println!("Starting");
     if forced {
-        stop();
+        stop(true);
     }
     if is_running() {
         println!("Seems to be running. Consider stopping.");
@@ -55,29 +61,54 @@ fn start(forced: bool) {
     std::process::exit(0)
 }
 
-fn stop() {
+fn stop(forced: bool) {
     println!("Stopping");
-    if !is_running() {
-        println!("Seems to be not running. Try starting.");
-        std::process::exit(0)
+    if !forced {
+        if !is_running() {
+            println!("Seems to be not running. Try starting.");
+            std::process::exit(0)
+        }
+        let mut pid_file = File::open(PID_FILE_PATH).expect("Failed to open pid file.");
+        let pid_file_content = &mut "".to_string();
+        pid_file
+            .read_to_string(pid_file_content)
+            .expect("Failed to read pid file.");
+        let pid = pid_file_content
+            .parse::<usize>()
+            .expect("Pin file content should parse to a number");
+        let binding = System::new_with_specifics(RefreshKind::with_processes(
+            RefreshKind::default(),
+            Default::default(),
+        ));
+        let pid = Pid::from(pid);
+        let process = binding.process(pid);
+        match process {
+            None => {
+                println!("No process with pid {}", pid);
+                std::fs::remove_file(PID_FILE_PATH).expect("Couldn't remove pid file");
+                println!("Stopped.");
+            }
+            Some(process) => {
+                process.kill();
+                std::fs::remove_file(PID_FILE_PATH).expect("Couldn't remove pid file");
+                println!("Stopped pid: {}", pid);
+            }
+        }
     }
-    let mut pid_file = File::open(PID_FILE_PATH).expect("Failed to open pid file.");
-    let pid_file_content = &mut "".to_string();
-    pid_file
-        .read_to_string(pid_file_content)
-        .expect("Failed to read pid file.");
-    let pid = pid_file_content
-        .parse::<usize>()
-        .expect("Pin file content should parse to a number");
-    let binding = System::new_with_specifics(RefreshKind::with_processes(
-        RefreshKind::default(),
-        Default::default(),
-    ));
-    let pid = Pid::from(pid);
-    let process = binding.process(pid).expect("Pid process should exist.");
-    process.kill();
-    std::fs::remove_file(PID_FILE_PATH).expect("Couldn't remove pid file");
-    println!("Stopped pid: {}", pid);
+    else {
+        if is_running() {
+            stop(false)
+        } else {
+            let system_binding = System::new_with_specifics(RefreshKind::with_processes(
+                RefreshKind::default(),
+                Default::default(),
+            ));
+            let processes = system_binding.processes_by_name(PROCESS_NAME);
+            processes.for_each(|process| -> () {
+                process.kill();
+            })
+        }
+    }
 }
 
 fn status() {
@@ -93,7 +124,7 @@ fn restart() {
         println!("Seems not to be running. Consider starting.");
         std::process::exit(0)
     }
-    stop();
+    stop(false);
     start(false);
 }
 
