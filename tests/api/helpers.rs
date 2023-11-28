22413f64fef24ff7fd708aff634b228ce3a636f5
Emanuel Minetti <e.minetti@posteo.de>
2023-11-28 22:03:27 +0100

    Try to repair tests for windows.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,4 +1,4 @@
 pub async fn spawn_app() {
     let server = actix_rest_vue_setup::run().expect("Failed to bind address.");
-    std::mem::drop(tokio::spawn(server));
+    drop(tokio::spawn(server));
 }
