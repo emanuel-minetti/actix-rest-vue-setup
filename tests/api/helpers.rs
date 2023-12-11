0bf45c345953edf8e45fe6f3e297b09a69e2550a
Emanuel Minetti <e.minetti@posteo.de>
2023-12-11 22:29:20 +0100

    On the way to linux testing.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -4,7 +4,7 @@ pub fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = actix_rest_vue_setup::run(listener).expect("Failed to bind address.");
-    //drop(tokio::spawn(server));
-    let _ = tokio::spawn(server);
+    //let _ = tokio::spawn(server);
+    drop(tokio::spawn(server));
     format!("http://127.0.0.1:{}", port)
 }
