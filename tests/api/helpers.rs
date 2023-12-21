1260e4b54a08a73f4abd1a9340f034ba5c868b8c
Emanuel Minetti <e.minetti@posteo.de>
2023-12-21 01:16:39 +0100

    Added test stub.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,10 +1,11 @@
 use actix_rest_vue_setup::configuration::Settings;
+use actix_rest_vue_setup::startup_lib;
 use std::net::TcpListener;
 
 pub fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let app = actix_rest_vue_setup::startup_lib::run(listener);
+    let app = startup_lib::run(listener);
     drop(tokio::spawn(app.server.expect("Failed to bind address.")));
 
     TestApp {
---
+++ tests/api/logging.rs
@@ -0,0 +1,23 @@
+use crate::helpers::spawn_app;
+use std::fs::OpenOptions;
+
+#[tokio::test]
+async fn writing_to_logfile_works() {
+    // Arrange
+    let test_app = spawn_app();
+    let path = test_app.settings.log_settings().path() + ".log";
+    let file = OpenOptions::new()
+        .write(true)
+        .create(false)
+        .open(path)
+        .expect("Failed to open logfile for writing");
+    file.set_len(0).expect("Clearing logfile failed");
+    let client = reqwest::Client::new();
+    // Act
+    let _ = client
+        .get(format!("{}/", &test_app.address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+}
