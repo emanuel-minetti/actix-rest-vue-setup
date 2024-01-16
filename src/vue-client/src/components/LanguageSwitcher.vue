8cc425ea9cd08b44dd9570bfd40713979f181784
Emanuel Minetti <e.minetti@posteo.de>
2024-01-16 21:39:24 +0100

    Keyboard handling the language switcher is working.

--- src/vue-client/src/components/LanguageSwitcher.vue
+++ src/vue-client/src/components/LanguageSwitcher.vue
@@ -1,9 +1,16 @@
 <template>
-  <span :class="`fi fi-${flagAbbrev}`" @click="showSelect = !showSelect"></span>
+  <div
+    tabindex="0"
+    :class="`fi fi-${flagAbbrev}`"
+    @click="flagClickedHandler"
+    @keyup="listenToEnterHandler"
+  ></div>
   <select
+    id="ls-select"
     v-if="showSelect"
     class="form-select"
     @mouseup="selectMouseupHandler"
+    @change="optionMouseupHandler"
     @blur="showSelect = false"
   >
     <option
@@ -19,7 +26,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, nextTick, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 import Translations from '@/i18n/Translations';
@@ -28,6 +35,19 @@ import type { LocaleAbbrev } from '@/i18n/Translations';
 const { t, locale } = useI18n();
 const supportedLocales = Translations.supportedLocales;
 const showSelect = ref(false);
+
+const flagClickedHandler = async () => {
+  showSelect.value = !showSelect.value;
+  await nextTick();
+  const selectElement = document.getElementById('ls-select');
+  selectElement!.focus();
+};
+const listenToEnterHandler = (event: KeyboardEvent) => {
+  const target = <HTMLElement>event.target;
+  if (event.key == 'Enter') {
+    target.click();
+  }
+};
 const selectMouseupHandler = async (event: Event) => {
   const target: HTMLSelectElement = <HTMLSelectElement>event.target!;
   let flag = target.dataset.flag;
