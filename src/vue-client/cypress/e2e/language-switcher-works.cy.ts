4fa28e1f943a07113005598daebe37cfddecbfd3
Emanuel Minetti <e.minetti@posteo.de>
2024-01-19 00:41:11 +0100

    Added three tests.

--- src/vue-client/cypress/e2e/language-switcher-works.cy.ts
+++ src/vue-client/cypress/e2e/language-switcher-works.cy.ts
@@ -1,22 +1,80 @@
 describe('Visiting the app', () => {
-  it('shows a flag for the default language', () => {
+  const DEFAULT_LANG = 'en';
+  const FLAG_ABBREV = DEFAULT_LANG == 'en' ? 'gb' : DEFAULT_LANG;
+
+  beforeEach(() => {
     cy.intercept('/api/config', () => {}).as('getConfig');
-    cy.visit('').wait('@getConfig').get('.fi').should('have.class', 'fi-gb');
+  });
+
+  it('shows a flag for the default language', () => {
+    cy.visit('')
+      .wait('@getConfig')
+      .get('.fi')
+      .should('have.class', 'fi-' + FLAG_ABBREV);
   });
 
   it('shows a select for language switching after clicking the flag', () => {
-    cy.intercept('/api/config', () => {}).as('getConfig');
     cy.visit('')
       .wait('@getConfig')
       .get('#ls-select')
       .should('not.exist')
       .get('.fi')
-      .should('have.class', 'fi-gb')
+      .should('have.class', 'fi-' + FLAG_ABBREV)
       .click()
       .get('#ls-select')
       .should('exist')
       .get('body')
       .click()
+      .get('#ls-select')
+      .should('not.exist');
+  });
+
+  it('shows a select for language switching after pressing ENTER on the flag', () => {
+    cy.visit('')
+      .wait('@getConfig')
+      .get('#ls-select')
+      .should('not.exist')
+      .get('.fi')
+      .should('have.class', 'fi-' + FLAG_ABBREV)
+      .type('{enter}')
+      .get('#ls-select')
+      .should('exist')
+      .get('body')
+      .click()
+      .get('#ls-select')
+      .should('not.exist');
+  });
+
+  it('closes select for language switching after clicking the same language', () => {
+    cy.visit('')
+      .wait('@getConfig')
+      .get('#ls-select')
+      .should('not.exist')
+      .get('.fi')
+      .should('have.class', 'fi-' + FLAG_ABBREV)
+      .click()
+      .get('#ls-select')
+      .should('exist')
+      .select(DEFAULT_LANG)
+      .get('#ls-select')
+      .should('not.exist');
+  });
+
+  it('changes the flag and closes select for language switching after clicking another language', () => {
+    cy.visit('')
+      .wait('@getConfig')
+      .get('#ls-select')
+      .should('not.exist')
+      .get('.fi')
+      .should('have.class', 'fi-' + FLAG_ABBREV)
+      .click()
+      .get('#ls-select')
+      .should('exist')
+      .children()
+      .not('[value][value="' + DEFAULT_LANG + '"]:first')
+      .click({ force: true })
+      .get('.fi')
+      .should('not.have.class', 'fi-' + FLAG_ABBREV)
       .get('#ls-select')
       .should('not.exist');
   });
